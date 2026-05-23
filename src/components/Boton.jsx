51f1418915d93643cea60e9a766e0fd6189ebff5
LiquidArtDevelopers <aranaz@gmail.com>
2026-05-23 08:36:26 +0200

    first commit

---
+++ src/components/Boton.jsx
@@ -0,0 +1,12 @@
+// Boton es un componente visual muy sencillo.
+// De momento no tiene onClick porque los eventos se explicaran mas adelante.
+function Boton() {
+  return (
+    <button className="boton" type="button">
+      Ver temario
+    </button>
+  )
+}
+
+// Exportamos Boton para poder usarlo dentro de la pagina Inicio.
+export default Boton
